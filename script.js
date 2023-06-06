d01cea5d0e67103f3d887d945fab36929a148803
cokanmelih <melihcokan@icloud.com>
2023-06-06 20:45:27 +0300

    first version

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed =  new Typed('.multiple-text',{
+    strings: ['Backend','Frontend','FullStack.. Maybe ?'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000, 
+    loop: true
+})
