6fffd862c593e1fc74e47e4b2558e4692ed65365
cokanmelih <melihcokan@icloud.com>
2023-06-07 04:43:38 +0300

    feat: tetris ( with colorful issues)

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const typed =  new Typed('.multiple-text',{
-    strings: ['Backend','Frontend','FullStack.. Maybe ?'],
+    strings: ['Backend','Frontend','FullStack.. (Maybe ?)'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000, 
     loop: true
 })
+
+const asd =  setTimeout(() => {
+    new Typed('.tetris-text',{
+        strings: ['What about some...          Tetris ?'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000, 
+    })
+}, 2500);
